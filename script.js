62a863ec55c797a3c11f8a0621017857af26dd47
Pavlo Tymoshchuk <Pavlo25575@icloud.com>
2020-04-10 02:28:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// Зміна кнопки за натиском
+
+var btns = document.getElementsByClassName("gall-tab");
+var prl = document.getElementById("prl");
+
+for (var i = 1; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    prl.style.marginTop = '60vw';
+  });
+}
+
+btns[0].addEventListener("click", function() {
+  prl.style.marginTop = '130vw';
+});
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    $(this).css({transition:'0.8s'});
+  });
+
+
+// Сортування по типу
+
+  filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+  this.className += " active";
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+    }
+  }
+  element.className = arr1.join(" ");
+}
+}
+
+// Каруселька
